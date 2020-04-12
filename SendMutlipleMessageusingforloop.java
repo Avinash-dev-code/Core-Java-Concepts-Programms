fd5fd8d91f5669f97d14091d9311c055b95d9839
AvinashDev <55042628+Avinash-dev-code@users.noreply.github.com>
2020-04-13 00:30:22 +0530

    Update SendMutlipleMessageusingforloop.java

--- SendMutlipleMessageusingforloop.java
+++ SendMutlipleMessageusingforloop.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 
-class SendMutlipleMessageusinforloop 
+class SendMutlipleMessageusingforloop 
 {
 
    public static void main(String[] args) {
        System.out.println("Enter a message:");
        Scanner sc=new Scanner(System.in);
        String message=sc.nextLine();
-       for(;;){
+       for(int i=1;i<=12;i++){
            System.out.println(message);
 
 
