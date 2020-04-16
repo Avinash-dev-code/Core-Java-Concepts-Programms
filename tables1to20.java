f7585fd84d4271291f276ed79db5f9ee3206b613
AvinashDev <55042628+Avinash-dev-code@users.noreply.github.com>
2020-04-16 22:47:54 +0530

    addedd.........

---
+++ tables1to20.java
@@ -0,0 +1,16 @@
+
+public class tables1to20 {
+
+
+    public static void main(String[] args) {
+         
+        for(int n=1;n<=20;n++)
+        {
+            for(int j=1;j<=10;j++)
+            {
+                System.out.println(n+"*"+j+"="+n*j+" ");
+            }
+            
+        }
+    }
+}
